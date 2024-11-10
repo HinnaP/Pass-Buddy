5d1fc106911fadeaa7cec3ad4c41b12f17ebe896
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2024-11-09 23:51:13 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+ flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    is_verified = db.Column(db.Boolean, default=False)
+    verification_code = db.Column(db.String(6), nullable=True)
+    code_expiration = db.Column(db.DateTime, nullable=True)
+
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
